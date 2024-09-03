38d8fddc24035736b2ad459e307fca250d711a8f
Nagajaswanth05 <ramineninagajaswanth16@gmail.com>
2024-09-03 19:31:44 +0530

    Updated

--- AdminService/src/main/java/com/admin/AdminService/controller/AdminController.java
+++ AdminService/src/main/java/com/admin/AdminService/controller/AdminController.java
@@ -2,7 +2,9 @@ package com.admin.AdminService.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/admin")
