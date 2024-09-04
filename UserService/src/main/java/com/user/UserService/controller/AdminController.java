de81a3a14ff11b24735e9ef14dd357b739dfbb38
Nagajaswanth05 <ramineninagajaswanth16@gmail.com>
2024-09-04 16:34:11 +0530

    Updated restApiConventions

--- UserService/src/main/java/com/user/UserService/controller/AdminController.java
+++ UserService/src/main/java/com/user/UserService/controller/AdminController.java
@@ -1,7 +1,7 @@
 package com.user.UserService.controller;
 import com.user.UserService.dto.CountryDTO;
 import com.user.UserService.service.CountryService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,23 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
-
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/admins")
 public class AdminController
 {
-    @Autowired
     private CountryService countryService;
-
-    @GetMapping("/getCountries")
-    public ResponseEntity<List<CountryDTO>> getCountries() {
-        List<CountryDTO> countries = countryService.getAllCountries();
+    @GetMapping("/countries")
+    public ResponseEntity<List<CountryDTO>> fetchAllCountries() {
+        List<CountryDTO> countries = countryService.fetchCountries();
         return ResponseEntity.ok().body(countries);
     }
-    @PostMapping("/addCountries")
-    public ResponseEntity<CountryDTO> addCountry(@RequestBody CountryDTO countryDTO) {
-        CountryDTO savedCountry = countryService.addCountry(countryDTO);
+    @PostMapping("/countries")
+    public ResponseEntity<CountryDTO> saveCountry(@RequestBody CountryDTO countryDTO) {
+        CountryDTO savedCountry = countryService.saveCountry(countryDTO);
         return ResponseEntity.ok().body(savedCountry);
     }
-
 }
--- UserService/src/main/java/com/user/UserService/service/CountryServiceImpl.java
+++ UserService/src/main/java/com/user/UserService/service/CountryServiceImpl.java
@@ -3,29 +3,26 @@ package com.user.UserService.service;
 import com.user.UserService.dao.CountryRepository;
 import com.user.UserService.dto.CountryDTO;
 import com.user.UserService.entity.Country;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
 @Service
+@RequiredArgsConstructor
 public class CountryServiceImpl implements CountryService{
-    @Autowired
     private CountryRepository countryRepository;
-    @Autowired
     private ModelMapper modelMapper;
-    public List<CountryDTO> getAllCountries() {
+    public List<CountryDTO> fetchCountries() {
         return countryRepository.findAll().stream()
                 .map(country -> modelMapper.map(country, CountryDTO.class))
                 .collect(Collectors.toList());
     }
-    public CountryDTO addCountry(CountryDTO countryDTO) {
+    public CountryDTO saveCountry(CountryDTO countryDTO) {
 
         Optional<Country> existingCountry = countryRepository.findByName(countryDTO.getName());
         if (existingCountry.isPresent()) {
-
             return modelMapper.map(existingCountry.get(), CountryDTO.class);
         }
         Country country = modelMapper.map(countryDTO, Country.class);
