e62bf097b3abb8d2129c3843c2c8f4e5584c00c1
praveen_gittagoni <praveen-gittagoni@epam.com>
2024-09-05 20:16:38 +0530

    Updated

---
+++ UserService/src/main/java/com/user/userservice/entity/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.user.userservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+@Builder
+public class ApiResponse
+{
+    private String response;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    public ApiResponse() {
+        this.timestamp = LocalDateTime.now();
+    }
+}
