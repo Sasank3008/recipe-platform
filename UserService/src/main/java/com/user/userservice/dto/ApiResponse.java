3bbbdcf9523a0cc20e5223612c7cf8770827eac1
poorna_sasank <poorna_sasank@epam.com>
2024-09-09 22:43:05 +0530

    Merging Code .

--- UserService/src/main/java/com/user/userservice/dto/ApiResponse.java
+++ UserService/src/main/java/com/user/userservice/dto/ApiResponse.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 public class ApiResponse {
+    private String response;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
-    private String response;
 }
