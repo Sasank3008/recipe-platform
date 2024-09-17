3862bb5e22298bee2fde01b4ff4cc2932fcc0834
Roopini Gurram <roopini_gurram@epam.com>
2024-09-17 21:18:36 +0530

    Changed the file names.

--- NotificationService/src/main/java/com/notificationservice/NotificationServiceApplication.java
+++ NotificationService/src/main/java/com/notificationservice/NotificationServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotificationManagementServiceApplication {
+public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NotificationManagementServiceApplication.class, args);
+		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
 }
--- NotificationService/src/test/java/com/notificationservice/NotificationServiceApplicationTests.java
+++ NotificationService/src/test/java/com/notificationservice/NotificationServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class NotificationManagementServiceApplicationTests {
+public class NotificationServiceApplicationTests {
 
     @Test
     void contextLoads() {
