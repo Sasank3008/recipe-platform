e97272bb787c4b356a80d5293a4c7b48c576c881
Roopini Gurram <roopini_gurram@epam.com>
2024-09-27 17:32:19 +0530

    Removed all * import statements

--- RecipeService/src/main/java/com/recipe/recipeservice/controller/ReviewRatingController.java
+++ RecipeService/src/main/java/com/recipe/recipeservice/controller/ReviewRatingController.java
@@ -1,6 +1,11 @@
 package com.recipe.recipeservice.controller;
 
-import com.recipe.recipeservice.dto.*;
+import com.recipe.recipeservice.dto.AddReviewDTO;
+import com.recipe.recipeservice.dto.AverageRatingDTO;
+import com.recipe.recipeservice.dto.RecipeReviewDTO;
+import com.recipe.recipeservice.dto.ReviewDataDTO;
+import com.recipe.recipeservice.dto.ReviewResponseDTO;
+import com.recipe.recipeservice.dto.UpdateReviewDTO;
 import com.recipe.recipeservice.entity.ReviewRating;
 import com.recipe.recipeservice.exception.ResourceNotFoundException;
 import com.recipe.recipeservice.exception.ReviewNotFoundException;
@@ -9,9 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("recipes/api/reviews")
 public class ReviewRatingController {
+
     @Autowired
     private ReviewRatingService reviewRatingService;
 
@@ -22,7 +29,7 @@ public class ReviewRatingController {
     }
 
     @PutMapping
-    public ResponseEntity<ReviewRating> editReview(@RequestBody UpdateReviewDTO updateReviewDTO)throws ResourceNotFoundException {
+    public ResponseEntity<ReviewRating> editReview(@RequestBody UpdateReviewDTO updateReviewDTO) throws ResourceNotFoundException {
         ReviewRating updatedReview = reviewRatingService.updateReview(updateReviewDTO);
         return new ResponseEntity<>(updatedReview, HttpStatus.OK);
     }
@@ -48,17 +55,16 @@ public class ReviewRatingController {
         return ResponseEntity.ok(reviewResponseDTO);
     }
 
-
     @DeleteMapping("/{recipeId}")
     public ResponseEntity<Void> deleteReview(@PathVariable Long recipeId, @RequestHeader("userId") Long userId) {
         reviewRatingService.deleteReview(userId, recipeId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
     @GetMapping("/{recipeId}/averageRating")
-    public ResponseEntity<AverageRatingDTO> getAverageRating(@PathVariable Long recipeId)throws ResourceNotFoundException {
+    public ResponseEntity<AverageRatingDTO> getAverageRating(@PathVariable Long recipeId) throws ResourceNotFoundException {
         Double averageRating = reviewRatingService.getAverageRating(recipeId);
         AverageRatingDTO averageRatingDTO = new AverageRatingDTO(averageRating);
         return new ResponseEntity<>(averageRatingDTO, HttpStatus.OK);
     }
-
 }
--- RecipeService/src/main/java/com/recipe/recipeservice/entity/ReviewRating.java
+++ RecipeService/src/main/java/com/recipe/recipeservice/entity/ReviewRating.java
@@ -1,6 +1,12 @@
 package com.recipe.recipeservice.entity;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -34,6 +40,4 @@ public class ReviewRating {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
-
-
 }
